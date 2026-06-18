866b759b67d97cd07fdafd24eedd8b3985923ff7
gavin <gavin_wang@berkeley.edu>
2026-06-18 14:58:54 -0700

    Gda test

---
+++ tests/test_gda.py
@@ -0,0 +1,22 @@
+import numpy as np
+from models.gaussian_discrimant_analysis import GDA
+
+X1 = np.random.randn(50, 2) + np.array([2, 2])
+y1 = np.ones(50, dtype = int)
+
+X0 =np.random.rand(50, 2) + np.array([-2, 2])
+y0 = np.zeros(50, dtype = int)
+
+X = np.vstack((X1, X0)) 
+y = np.concatenate((y1, y0))
+
+model = GDA()
+
+model.fit(X, y)
+
+predictions = model.predict(X)
+
+accuracy = np.mean(predictions == y)
+print(accuracy)
+assert(accuracy > 0.9)
+
