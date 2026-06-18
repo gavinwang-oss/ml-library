bd412eee3ae07b84f8484c0e267f0f3e771aec00
gavin <gavin_wang@berkeley.edu>
2026-06-18 09:00:09 -0700

    fixed naive bayes test case

--- tests/test_naive_bayes.py
+++ tests/test_naive_bayes.py
@@ -5,7 +5,7 @@ X1 = (np.random.rand(50, 10) < 0.7).astype(int)
 y1 = np.ones(50, dtype = int)
 
 X0 = (np.random.rand(50, 10) < 0.3).astype(int)
-y0 = np.ones(50, dtype = int)
+y0 = np.zeros(50, dtype = int)
 
 X = np.vstack((X1, X0))
 y = np.concatenate((y1, y0))
