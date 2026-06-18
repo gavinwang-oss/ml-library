b3cbdd75ec8dc7edd4a70ca6ba5ee12a5e9ae255
gavin <gavin_wang@berkeley.edu>
2026-06-18 15:01:24 -0700

    anotehr test bug

--- tests/test_gda.py
+++ tests/test_gda.py
@@ -4,7 +4,7 @@ from models.gaussian_discrimant_analysis import GDA
 X1 = np.random.randn(50, 2) + np.array([2, 2])
 y1 = np.ones(50, dtype = int)
 
-X0 =np.random.randn(50, 2) + np.array([-2, 2])
+X0 =np.random.randn(50, 2) + np.array([-2, -2])
 y0 = np.zeros(50, dtype = int)
 
 X = np.vstack((X1, X0)) 
